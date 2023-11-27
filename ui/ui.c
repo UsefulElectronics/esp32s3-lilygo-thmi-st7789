882ef166cf59a461c76a415bf45feaae792277e2
unknown <wardzx@hotmail.com>
2023-11-27 23:15:15 +0300

    UI filese has been updated.

--- ui/ui.c
+++ ui/ui.c
@@ -11,7 +11,10 @@
 // SCREEN: ui_Screen1
 void ui_Screen1_screen_init(void);
 lv_obj_t * ui_Screen1;
+lv_obj_t * ui_Image1;
 lv_obj_t * ui_Image2;
+lv_obj_t * ui_Image3;
+lv_obj_t * ui_Label1;
 lv_obj_t * ui____initial_actions0;
 
 ///////////////////// TEST LVGL SETTINGS ////////////////////
--- ui/ui.h
+++ ui/ui.h
@@ -17,10 +17,15 @@ extern "C" {
 // SCREEN: ui_Screen1
 void ui_Screen1_screen_init(void);
 extern lv_obj_t * ui_Screen1;
+extern lv_obj_t * ui_Image1;
 extern lv_obj_t * ui_Image2;
+extern lv_obj_t * ui_Image3;
+extern lv_obj_t * ui_Label1;
 extern lv_obj_t * ui____initial_actions0;
 
-LV_IMG_DECLARE(ui_img_useful_electronics_png);    // assets\USEFUL_ELECTRONICS.png
+LV_IMG_DECLARE(ui_img_background_png);    // assets\background.png
+LV_IMG_DECLARE(ui_img_status_png);    // assets\status.png
+LV_IMG_DECLARE(ui_img_rhomboid_png);    // assets\rhomboid.png
 
 void ui_init(void);
 
